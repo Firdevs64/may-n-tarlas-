d9139a899a192100040ae53b8d6ed014dda6ad85
Firdevs Köse <147314210+Firdevs64@users.noreply.github.com>
2024-12-03 11:40:30 +0300

    Add files via upload

---
+++ MayınTarlası/MayınTarlası/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace MayınTarlası
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.oyunToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kolayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ortaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oyunToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(897, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // oyunToolStripMenuItem
+            // 
+            this.oyunToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kolayToolStripMenuItem,
+            this.ortaToolStripMenuItem,
+            this.zorToolStripMenuItem});
+            this.oyunToolStripMenuItem.Name = "oyunToolStripMenuItem";
+            this.oyunToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.oyunToolStripMenuItem.Text = "Oyun";
+            // 
+            // kolayToolStripMenuItem
+            // 
+            this.kolayToolStripMenuItem.Name = "kolayToolStripMenuItem";
+            this.kolayToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.kolayToolStripMenuItem.Text = "Kolay";
+            this.kolayToolStripMenuItem.Click += new System.EventHandler(this.kolayToolStripMenuItem_Click);
+            // 
+            // ortaToolStripMenuItem
+            // 
+            this.ortaToolStripMenuItem.Name = "ortaToolStripMenuItem";
+            this.ortaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ortaToolStripMenuItem.Text = "Orta";
+            this.ortaToolStripMenuItem.Click += new System.EventHandler(this.ortaToolStripMenuItem_Click);
+            // 
+            // zorToolStripMenuItem
+            // 
+            this.zorToolStripMenuItem.Name = "zorToolStripMenuItem";
+            this.zorToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.zorToolStripMenuItem.Text = "Zor";
+            this.zorToolStripMenuItem.Click += new System.EventHandler(this.zorToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Puan : 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mayın : 0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(2, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Boyut : 30x30";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 30;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 27F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(102, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 30;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(780, 780);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 791);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem oyunToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kolayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ortaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zorToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ MayınTarlası/MayınTarlası/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MayınTarlası
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ArrayList mayinlar = new ArrayList();
+
+
+        private void kolayToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mayinlar.Clear();
+            tableLayoutPanel1.Controls.Clear();
+            gPuan = 0;
+            int oMayin = 100;  //Kaç tane mayın oluşturduğunu yazdırıyor.
+            int oTarla = 900;
+            tableLayoutPanel1.ColumnCount = 30;
+            label2.Text = "Mayın : " + oMayin;
+            label3.Text = "Boyut : " + tableLayoutPanel1.ColumnCount + " x " + tableLayoutPanel1.ColumnCount;
+            Random rast = new Random();
+            int sayi = 0;
+            for(int i = 0; i < oMayin; i++)
+            {
+                uret:
+                sayi = rast.Next(0, oTarla);
+                if (mayinlar.Contains(sayi))
+                {
+                    goto uret;
+                }
+                else
+                {
+                    mayinlar.Add(sayi);
+                }
+            }
+            for (int i = 0; i < oTarla; i++)
+            {
+                Button button = new Button();
+                button.Size = new Size(26, 26);
+                if (mayinlar.Contains(i))
+                {
+                    button.Tag = -1;
+                }
+                else
+                {
+                    button.Tag = rast.Next(1, 4);
+                }
+                button.Click += button_Click;
+                tableLayoutPanel1.Controls.Add(button);
+            }
+            
+
+
+        }
+        int gPuan = 0;
+        private void button_Click(object sender, EventArgs e)
+        {
+            
+            Button tiklanan = (Button)sender;
+            if (tiklanan.Text == "")
+            {
+                if (int.Parse(tiklanan.Tag.ToString()) == -1)
+                {
+                    tiklanan.BackgroundImage = Image.FromFile("mayin.png");
+                    tiklanan.BackColor = Color.Red;
+
+                    for (int i = 0; i < tableLayoutPanel1.Controls.Count; i++)
+                    {
+                        if (int.Parse(tableLayoutPanel1.Controls[i].Tag.ToString()) == -1)
+                        {
+                            tableLayoutPanel1.Controls[i].BackgroundImage = Image.FromFile("mayin.png");
+                            tableLayoutPanel1.Controls[i].Enabled = false;
+                        }
+                        else
+                        {
+                            tableLayoutPanel1.Controls[i].Enabled = false;
+                            tableLayoutPanel1.Controls[i].Text = tableLayoutPanel1.Controls[i].Tag.ToString();
+                        }
+                    }
+                }
+                else
+                {
+                    tiklanan.Text = tiklanan.Tag.ToString();
+                    gPuan += int.Parse(tiklanan.Tag.ToString());
+                    label1.Text = "Puan: " + gPuan;
+
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ortaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mayinlar.Clear();
+            tableLayoutPanel1.Controls.Clear();
+            gPuan = 0;
+            int oMayin = 200;  //Kaç tane mayın oluşturduğunu yazdırıyor.
+            int oTarla = 900;
+            tableLayoutPanel1.ColumnCount = 30;
+            label2.Text = "Mayın : " + oMayin;
+            label3.Text = "Boyut : " + tableLayoutPanel1.ColumnCount + " x " + tableLayoutPanel1.ColumnCount;
+            Random rast = new Random();
+            int sayi = 0;
+            for (int i = 0; i < oMayin; i++)
+            {
+            uret:
+                sayi = rast.Next(0, oTarla);
+                if (mayinlar.Contains(sayi))
+                {
+                    goto uret;
+                }
+                else
+                {
+                    mayinlar.Add(sayi);
+                }
+            }
+            for (int i = 0; i < oTarla; i++)
+            {
+                Button button = new Button();
+                button.Size = new Size(30, 30);
+                if (mayinlar.Contains(i))
+                {
+                    button.Tag = -1;
+                }
+                else
+                {
+                    button.Tag = rast.Next(1, 4);
+                }
+                button.Click += button_Click;
+                tableLayoutPanel1.Controls.Add(button);
+
+            }
+            
+        }
+
+        private void zorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mayinlar.Clear();
+            tableLayoutPanel1.Controls.Clear();
+            gPuan = 0;
+            int oMayin = 300;  //Kaç tane mayın oluşturduğunu yazdırıyor.
+            int oTarla = 900;
+            tableLayoutPanel1.ColumnCount = 30;
+            label2.Text = "Mayın : " + oMayin;
+            label3.Text = "Boyut : " + tableLayoutPanel1.ColumnCount + " x " + tableLayoutPanel1.ColumnCount;
+            Random rast = new Random();
+            int sayi = 0;
+            for (int i = 0; i < oMayin; i++)
+            {
+            uret:
+                sayi = rast.Next(0, oTarla);
+                if (mayinlar.Contains(sayi))
+                {
+                    goto uret;
+                }
+                else
+                {
+                    mayinlar.Add(sayi);
+                }
+            }
+            for (int i = 0; i < oTarla; i++)
+            {
+                Button button = new Button();
+                button.Size = new Size(30, 30);
+                if (mayinlar.Contains(i))
+                {
+                    button.Tag = -1;
+                }
+                else
+                {
+                    button.Tag = rast.Next(1, 4);
+                }
+                button.Click += button_Click;
+                tableLayoutPanel1.Controls.Add(button);
+
+            }
+            
+        }
+    }
+}
